685c85d2b6dd16b2ef3eaf6fa8142108d8086d81
chanchanwoong <cksdnddlek99@gmail.com>
2024-11-16 23:57:43 +0900

    add: PostgreSQL 엔티티 클래스, MongoDB 다큐먼트 클래스 추가

--- src/main/java/com/performancetest/chanwoong/domain/RoundDataMongoDB.java
+++ src/main/java/com/performancetest/chanwoong/domain/RoundDataMongoDB.java
@@ -1,4 +1,34 @@
 package com.performancetest.chanwoong.domain;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "round_info")
 public class RoundDataMongoDB {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roundInfoId;
+
+    private String auctionUuid;
+    private Integer round;
+    private LocalDateTime roundStartTime;
+    private LocalDateTime roundEndTime;
+    private BigDecimal incrementUnit;
+    private BigDecimal price;
+    private Boolean isActive;
+    private int numberOfParticipants;
+    private int leftNumberOfParticipants;
+    private LocalDateTime createdAt;
+    private LocalDateTime auctionEndTime;
+    private Boolean isLastRound;
+    private Boolean endStatus;
 }
--- src/main/java/com/performancetest/chanwoong/domain/RoundDataPostgreSQL.java
+++ src/main/java/com/performancetest/chanwoong/domain/RoundDataPostgreSQL.java
@@ -1,4 +1,32 @@
 package com.performancetest.chanwoong.domain;
 
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Document(collation = "round_info")
 public class RoundDataPostgreSQL {
+
+    @Id
+    private String roundInfoId;
+
+    private String auctionUuid;
+    private Integer round;
+    private LocalDateTime roundStartTime;
+    private LocalDateTime roundEndTime;
+    private BigDecimal incrementUnit;
+    private BigDecimal price;
+    private Boolean isActive;
+    private int numberOfParticipants;
+    private int leftNumberOfParticipants;
+    private LocalDateTime createdAt;
+    private LocalDateTime auctionEndTime;
+    private Boolean isLastRound;
+    private Boolean endStatus;
 }
