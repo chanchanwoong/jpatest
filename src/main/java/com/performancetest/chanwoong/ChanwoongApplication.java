47ea6444da2d6d456ac79502238fb137ca81c9a9
chanchanwoong <cksdnddlek99@gmail.com>
2024-11-16 16:08:55 +0900

    project generate

---
+++ src/main/java/com/performancetest/chanwoong/ChanwoongApplication.java
@@ -0,0 +1,13 @@
+package com.performancetest.chanwoong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanwoongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanwoongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/performancetest/chanwoong/ChanwoongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performancetest.chanwoong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanwoongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
